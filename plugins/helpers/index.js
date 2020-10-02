e3c3b1eadae80216407c49b2de24e05cab302233
Ec_ <dmatis01@gmail.com>
2020-10-02 13:08:34 +0200

    Added Byte and Enum helper classes

--- plugins/helpers/index.js
+++ plugins/helpers/index.js
@@ -58,7 +58,67 @@ class Recent {
     }
 }
 
+class Byte {
+    constructor(data) {
+        if (!isArray(data)) throw new Error("Byte input needs to be array");
+
+        this.data = new Map();
+        var index = 1;
+
+        data.forEach(value => {
+            this.data.set(value, index);
+            index = index * 2;
+        });
+
+        this.getBit = this.getBit.bind(this);
+        this.getByte = this.getByte.bind(this);
+        this.getValue = this.getValue.bind(this);
+        this.getMap = this.getMap.bind(this);
+
+        return this;
+    }
+
+    getMap() {
+        return this.data;
+    }
+
+    getBit(bit) {
+        var value = this.data.get(bit);
+        return value ? value : false;
+    }
+
+    getValue() {
+        return obj => Object.values(this.data).reduce((a, b) => a + b);
+    }
+
+    getByte() {
+        return (dec >>> this.getByte()).toString(2);
+    }
+}
+
+function isNumber(n) {
+    return !isNaN(parseFloat(n)) && !isNaN(n - 0)
+}
+
+class Enum {
+    constructor(data) {
+        var rval = {};
+        var index = 0;
+        for (var i in data) {
+            if (isObject(data[i])) {
+                rval[Object.keys(data[i])[0]] = index = Object.values(data[i])[0];
+            } else {
+                rval[data[i]] = index;
+            }
+            index++;
+        }
+        return Object.freeze(rval);
+    }
+}
+
 module.exports = {
     Recent: Recent,
     Lock: Lock,
+    Enum: Enum,
+    Byte: Byte,
 };
