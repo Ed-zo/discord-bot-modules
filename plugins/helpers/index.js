ded4a0f3d247082b92741a1389101b0afe939baf
Ec_ <dmatis01@gmail.com>
2020-10-02 13:14:08 +0200

    Created more helper functions
    Helper functions:
      - isObject
      - isClass
      - isArray
      - isNumber
      - parseItems
      - parseError
      - responseError
      - responseOk

--- plugins/helpers/index.js
+++ plugins/helpers/index.js
@@ -1,3 +1,27 @@
+function to(promise) {
+    return promise
+        .then(data => {
+            return [null, data];
+        })
+        .catch(err => [err]);
+}
+
+function limitLength(what, min, max) {
+    if (typeof what != "string")
+        return false;
+
+    if (!what.length)
+        return false;
+
+    if (what.length > max)
+        return false;
+
+    if (what.length < min)
+        return false;
+
+    return true;
+}
+
 class Lock {
     constructor(limit) {
         this.locks = [];
@@ -32,54 +56,38 @@ class Lock {
     }
 }
 
-class Recent {
-    constructor(keep, key) {
-        this.keep = keep;
-        this.recent = [];
-        this.key = key;
-        return this;
-    }
 
-    add(thing) {
-        if (this.recent.filter(a => a[this.key] == thing[this.key]).length > 0)
-            return false;
 
-        if (this.recent.length == this.keep) this.recent.pop();
+var isObject = object => {
+    return typeof object == "object" && !Array.isArray(object);
+};
 
-        this.recent.unshift(thing);
-    }
+var isArray = object => {
+    return typeof object == "object" && Array.isArray(object);
+};
 
-    get() {
-        return this.recent.map(a => {
-            var b = {...a }
-            delete b.key;
-            return b;
-        });
-    }
+var isClass = v => {
+    return typeof v === "function" && /^\s*class\s+/.test(v.toString());
+};
+
+function isNumber(n) {
+    return !isNaN(parseFloat(n)) && !isNaN(n - 0)
 }
 
-function to(promise) {
-    return promise
-        .then(data => {
-            return [null, data];
-        })
-        .catch(err => [err]);
-}
-
-function limitLength(what, min, max) {
-    if (typeof what != "string")
-        return false;
-
-    if (!what.length)
-        return false;
-
-    if (what.length > max)
-        return false;
-
-    if (what.length < min)
-        return false;
-
-    return true;
+class Enum {
+    constructor(data) {
+        var rval = {};
+        var index = 0;
+        for (var i in data) {
+            if (isObject(data[i])) {
+                rval[Object.keys(data[i])[0]] = index = Object.values(data[i])[0];
+            } else {
+                rval[data[i]] = index;
+            }
+            index++;
+        }
+        return Object.freeze(rval);
+    }
 }
 
 class Byte {
@@ -120,46 +128,6 @@ class Byte {
     }
 }
 
-function isNumber(n) {
-    return !isNaN(parseFloat(n)) && !isNaN(n - 0)
-}
-
-class Enum {
-    constructor(data) {
-        var rval = {};
-        var index = 0;
-        for (var i in data) {
-            if (isObject(data[i])) {
-                rval[Object.keys(data[i])[0]] = index = Object.values(data[i])[0];
-            } else {
-                rval[data[i]] = index;
-            }
-            index++;
-        }
-        return Object.freeze(rval);
-    }
-}
-
-YouTubeGetID = function(url) {
-    var ID = '';
-    console.log(url, "TEST")
-    url = url.replace(/(>|<)/gi, '').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
-    if (url[2] !== undefined) {
-        ID = url[2].split(/[^0-9a-z_\-]/i);
-        ID = ID[0];
-        return ID;
-    }
-    return url;
-}
-
-function arrayContainsArray(superset, subset) {
-    if (!superset || !subset)
-        return false;
-    return subset.every(function(value) {
-        return superset.indexOf(value) >= 0;
-    });
-}
-
 function chunkify(arr, len) {
 
     var chunks = [],
@@ -183,15 +151,77 @@ async function asyncForEach(array, callback) {
     }
 }
 
+class Recent {
+    constructor(keep, key) {
+        this.keep = keep;
+        this.recent = [];
+        this.key = key;
+        return this;
+    }
+
+    add(thing) {
+        if (this.recent.filter(a => a[this.key] == thing[this.key]).length > 0)
+            return false;
+
+        if (this.recent.length == this.keep) this.recent.pop();
+
+        this.recent.unshift(thing);
+    }
+
+    get() {
+        return this.recent.map(a => {
+            var b = {...a }
+            delete b.key;
+            return b;
+        });
+    }
+}
+
+function parseItems(string) {
+    var items = string.split(";");
+    return items.map((pair) => {
+        var k = pair.split(":");
+        return {
+            id: Number(k[0]),
+            permanentEnchant: Number(k[1]),
+        };
+    });
+}
+
+function responseError(res, toSend) {
+    res.status(403);
+    res.json({
+        data: null,
+        error: toSend,
+    });
+}
+
+function responseOk(res, toSend) {
+    res.status(200);
+    res.json({
+        data: toSend,
+        error: null,
+    });
+}
+
+
 module.exports = {
-    Recent: Recent,
-    Lock: Lock,
+    to: to,
+    arrayContainsArray: arrayContainsArray,
     Enum: Enum,
+    isObject: isObject,
+    isClass: isClass,
+    isArray: isArray,
     Byte: Byte,
     YouTubeGetID: YouTubeGetID,
-    arrayContainsArray: arrayContainsArray,
-    to: to,
-    limitLength: limitLength,
+    isNumber: isNumber,
     chunkify: chunkify,
-    sleep: sleep
+    sleep: sleep,
+    asyncForEach: asyncForEach,
+    Recent: Recent,
+    parseItems: parseItems,
+    responseError: responseError,
+    responseOk: responseOk,
+    Lock: Lock,
+    limitLength: limitLength
 };
